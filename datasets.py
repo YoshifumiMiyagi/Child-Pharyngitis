2322b38f6b0572fd179872a81b1ea502739be44a
yoshifumimiya <49806292+YoshifumiMiyagi@users.noreply.github.com>
2026-05-11 00:55:30 +0900

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -1,10 +1,18 @@
 import torch
+import numpy as np
+
 from torch.utils.data import Dataset
 from PIL import Image
 
 
 class PharyngitisImageDataset(Dataset):
-    def __init__(self, df, transform=None, target_col="bacterial_ratio"):
+
+    def __init__(
+        self,
+        df,
+        transform=None,
+        target_col="bacterial_ratio"
+    ):
         self.df = df.reset_index(drop=True)
         self.transform = transform
         self.target_col = target_col
@@ -13,18 +21,37 @@ class PharyngitisImageDataset(Dataset):
         return len(self.df)
 
     def __getitem__(self, idx):
+
         row = self.df.iloc[idx]
 
-        img = Image.open(row["image_path"]).convert("RGB")
-        if self.transform:
-            img = self.transform(img)
+        img = Image.open(
+            row["image_path"]
+        ).convert("RGB")
 
-        y = torch.tensor(row[self.target_col], dtype=torch.float32)
+        img = np.array(img)
+
+        if self.transform:
+            img = self.transform(
+                image=img
+            )["image"]
+
+        y = torch.tensor(
+            row[self.target_col],
+            dtype=torch.float32
+        )
+
         return img, y
 
 
 class PharyngitisFusionDataset(Dataset):
-    def __init__(self, df, tabular_cols, transform=None, target_col="bacterial_ratio"):
+
+    def __init__(
+        self,
+        df,
+        tabular_cols,
+        transform=None,
+        target_col="bacterial_ratio"
+    ):
         self.df = df.reset_index(drop=True)
         self.tabular_cols = tabular_cols
         self.transform = transform
@@ -34,16 +61,30 @@ class PharyngitisFusionDataset(Dataset):
         return len(self.df)
 
     def __getitem__(self, idx):
+
         row = self.df.iloc[idx]
 
-        img = Image.open(row["image_path"]).convert("RGB")
+        img = Image.open(
+            row["image_path"]
+        ).convert("RGB")
+
+        img = np.array(img)
+
         if self.transform:
-            img = self.transform(img)
+            img = self.transform(
+                image=img
+            )["image"]
 
         x_tab = torch.tensor(
-            row[self.tabular_cols].astype(float).values,
+            row[self.tabular_cols]
+            .astype(float)
+            .values,
             dtype=torch.float32
         )
 
-        y = torch.tensor(row[self.target_col], dtype=torch.float32)
+        y = torch.tensor(
+            row[self.target_col],
+            dtype=torch.float32
+        )
+
         return img, x_tab, y
